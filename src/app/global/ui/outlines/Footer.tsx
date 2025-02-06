027f554544a505fbacd543bc354b28ac2a0fdf7b
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-02-06 18:20:17 +0900

    .

---
+++ src/app/global/ui/outlines/Footer.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React from "react"
+import { styled } from "styled-components"
+import colors from "../../assets/styles/color"
+const { dark, light } = colors;
+
+const StyledFooter = styled.footer`
+  background: ${dark};
+  min-height: 200px;
+  color: ${light}
+`
+
+const Footer = () => {
+  return (<StyledFooter>
+    <div className="layout-width">사이트 하단 영역</div>
+  </StyledFooter>)
+}
+
+export default React.memo(Footer)
